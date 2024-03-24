87061dc18fc92f2c25589a3f8eb647ebefefc258
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-24 14:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let outputContainer = document.querySelector("#container")
-let unorderedList = document.querySelector("#band")
+let unorderedList = document.querySelector("#bands")
 
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let ignoreArticle = (str) => {
